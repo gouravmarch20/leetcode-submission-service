a8bc337dee38b1db7d88cea45b68c7ec5af13258
gourav <gaurav.mishra@kogo.ai>
2024-08-19 18:13:57 +0530

    env sample added

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT || 5000,
     ATLAS_DB_URL: process.env.ATLAS_DB_URL,
     NODE_ENV: process.env.NODE_ENV,
     REDIS_PORT: process.env.REDIS_PORT || "6379",
