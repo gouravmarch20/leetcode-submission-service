a0adcda2a33029b896d960940b10c1ecff4fd041
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-10-06 15:20:59 +0530

    socket url

--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -1,28 +1,35 @@
-const { Worker } = require('bullmq');
-const redisConnection = require('../config/redisConfig');
-const axios = require('axios');
+const { Worker } = require("bullmq");
+const redisConnection = require("../config/redisConfig");
+const axios = require("axios");
+const { PROBLEM_ADMIN_SERVICE_URL } = require("../config/serverConfig");
+
+const PROBLEM_ADMIN_API_URL = `${PROBLEM_ADMIN_SERVICE_URL}/sendPayload`;
 
 function evaluationWorker(queue) {
-    console.log(`evaluationWorker` )
-    
-    new Worker('EvaluationQueue', async job => {
-        if (job.name === 'EvaluationJob') {
-            console.log(`evaluationWorker_1` , job )
+  console.log(`evaluationWorker`);
 
-            try {
-                const response = await axios.post('http://localhost:4004/sendPayload', {
-                    userId: job.data.userId,
-                    payload: job.data
-                })
-                console.log(response);
-              console.log(job.data);
-            } catch(error) {
-                console.log(error)
-            }
+  new Worker(
+    "EvaluationQueue",
+    async (job) => {
+      if (job.name === "EvaluationJob") {
+        console.log(`evaluationWorker_1`, job);
+
+        try {
+          const response = await axios.post(PROBLEM_ADMIN_API_URL, {
+            userId: job.data.userId,
+            payload: job.data,
+          });
+          console.log(response);
+          console.log(job.data);
+        } catch (error) {
+          console.log(error);
         }
-    }, {
-        connection: redisConnection
-    });
+      }
+    },
+    {
+      connection: redisConnection,
+    }
+  );
 }
 
 module.exports = evaluationWorker;
