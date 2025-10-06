4cac2206afbff26e902eb70233ffbd7beb8ee332
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-10-06 22:42:37 +0530

    aws port

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const fastify = require('fastify')({ logger: true }); // calling the fastify constructor
-const app = require('./app');
-const connectToDB = require('./config/dbConfig');
-const serverConfig = require('./config/serverConfig');
-const errorHandler = require('./utils/errorHandler');
-const evaluationWorker = require('./workers/evaluationWorker');
+const fastify = require("fastify")({ logger: true }); // calling the fastify constructor
+const app = require("./app");
+const connectToDB = require("./config/dbConfig");
+const serverConfig = require("./config/serverConfig");
+const errorHandler = require("./utils/errorHandler");
+const evaluationWorker = require("./workers/evaluationWorker");
 
 fastify.register(app);
 fastify.setErrorHandler(errorHandler);
 
-fastify.listen({ port: serverConfig.PORT }, async (err) => {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1)
-    }
-    await connectToDB();
+fastify.listen({ port: serverConfig.PORT, host: "0.0.0.0" }, async (err) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  await connectToDB();
 
-    evaluationWorker("EvaluationQueue");
-    console.log(`Server up at port ${serverConfig.PORT}`);
+  evaluationWorker("EvaluationQueue");
+  console.log(`Server up at port ${serverConfig.PORT}`);
 });
