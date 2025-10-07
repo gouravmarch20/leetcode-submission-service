de8e325c8bdfd63c98cf6db1bbf7f48319e6c530
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-10-08 01:18:58 +0530

    fronend side

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const connectToDB = require("./config/dbConfig");
 const serverConfig = require("./config/serverConfig");
 const errorHandler = require("./utils/errorHandler");
 const evaluationWorker = require("./workers/evaluationWorker");
+const { FRONTEND_URL } = require("./config/serverConfig");
+const fastifyCors = require("@fastify/cors");
+
+console.log("FRONTEND_URL", FRONTEND_URL);
+
+
+fastify.register(fastifyCors, {
+  origin: [FRONTEND_URL, "http://localhost:5173"], // allow your React dev server
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+});
 
 fastify.register(app);
 fastify.setErrorHandler(errorHandler);
+
+
 
 fastify.listen({ port: serverConfig.PORT, host: "0.0.0.0" }, async (err) => {
   if (err) {
--- src/workers/evaluationWorker.js
+++ src/workers/evaluationWorker.js
@@ -1,9 +1,10 @@
 const { Worker } = require("bullmq");
 const redisConnection = require("../config/redisConfig");
 const axios = require("axios");
-const { PROBLEM_ADMIN_SERVICE_URL } = require("../config/serverConfig");
+const { SOCKET_SERVICE_URL } = require("../config/serverConfig");
 
-const PROBLEM_ADMIN_API_URL = `${PROBLEM_ADMIN_SERVICE_URL}/sendPayload`;
+// const PROBLEM_ADMIN_API_URL = SOCKET_SERVICE_URL.concat("/sendPayload");
+const PROBLEM_ADMIN_API_URL = `${SOCKET_SERVICE_URL}/sendPayload`;
 
 function evaluationWorker(queue) {
   console.log(`evaluationWorker`);
@@ -12,17 +13,14 @@ function evaluationWorker(queue) {
     "EvaluationQueue",
     async (job) => {
       if (job.name === "EvaluationJob") {
-        console.log(`evaluationWorker_1`, job);
 
         try {
           const response = await axios.post(PROBLEM_ADMIN_API_URL, {
             userId: job.data.userId,
             payload: job.data,
           });
-          console.log(response);
-          console.log(job.data);
         } catch (error) {
-          console.log(error);
+          console.log(error.message);
         }
       }
     },
